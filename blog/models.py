1c14ceb73113b1508346b044203540b0a4ff0238
aabuiluk <andrii.a.builuk@gmail.com>
2025-11-26 18:59:20 +0200

    Fix models

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    create_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
 
     def __str__(self):
