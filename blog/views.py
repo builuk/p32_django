2701a786dc20094e7405f8123ab223b38da2636d
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-01 17:47:34 +0200

    Shop Models

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
-
 def post_list(request):
-    """
-    Сторінка списку постів + форма створення нового поста.
-    """
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             Post.objects.create(
                 title=form.cleaned_data["title"],
                 content=form.cleaned_data["content"],
-                is_published=True,
+                is_published=False,
             )
             return redirect("blog:post_list")
     else:
         form = PostForm()
-
     posts = Post.objects.filter(is_published=True).order_by("-create_at")
     context = {"posts": posts, "form": form}
     return render(request, "blog/post_list.html", context)
