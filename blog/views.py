19ea33ffde6a47dc7f8adc90a667fe9e87577fe8
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-01 17:25:27 +0200

    Fix models

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
 
 def post_list(request):
-    # posts = Post.objects.all()
+    """
+    Сторінка списку постів + форма створення нового поста.
+    """
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            Post.objects.create(
+                title=form.cleaned_data["title"],
+                content=form.cleaned_data["content"],
+                is_published=True,
+            )
+            return redirect("blog:post_list")
+    else:
+        form = PostForm()
+
     posts = Post.objects.filter(is_published=True).order_by("-create_at")
-    context = {"posts": posts}
+    context = {"posts": posts, "form": form}
     return render(request, "blog/post_list.html", context)
 
 def post_detail(request, pk: int):
     post = get_object_or_404(Post, pk=pk, is_published=True)
     context = {"post": post}
     return render(request, "blog/post_detail.html", context)
-
-def postform_form_view(request):
-    result = None
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            result = form.cleaned_data
-    else:
-        form = PostForm()
-    return render(request, "blog/post_list.html", {"form": form, "result": result})
